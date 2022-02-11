c65fe8446ccfb5bf21fb74e7e26754fc3446ad9b
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-02-11 22:37:16 +0600

    Complate Testimonials

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log('No Errors From Js');
+
+
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('main-header');
+    let scrollTop = window.pageYOffset;
+
+    if (scrollTop > 120) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+
+});
